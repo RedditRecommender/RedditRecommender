fae7d9a1ee8369604ab4b78adeb335209ad2afc1
Joe <joebill79@gmail.com>
2017-11-21 19:52:45 -0600

    Grabbing user information

--- redditPraw.py
+++ redditPraw.py
@@ -2,6 +2,7 @@
 import praw
 from credentials import reddit
 
+'''
 subreddit = reddit.subreddit('ShowerThoughts');
 
 hot_posts = subreddit.hot(limit=15)
@@ -9,3 +10,13 @@ hot_posts = subreddit.hot(limit=15)
 for submission in hot_posts:
    if not submission.stickied:
       print(submission.title)
+'''
+
+
+# Waddup nerds here's how to grab some user info
+username = 'spez' # set whatever username you want here
+
+myRedditor = reddit.redditor(username) # Create a 'Redditor' object from that username
+for comment in myRedditor.comments.new(limit=10): # Get that user's comments
+   print(comment.body.split('\n', 1)[0][:79]) # The comment
+   print('Subreddit: ', comment.subreddit.display_name) # The subreddit the comment is on
