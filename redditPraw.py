12c3d38c097821c4b0254d4e1fd9cbfab60d5a2b
Joe <joebill79@gmail.com>
2017-11-21 18:39:10 -0600

    Cleanup the random shit

--- redditPraw.py
+++ redditPraw.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 import praw
-import random
 from credentials import reddit
 
 subreddit = reddit.subreddit('ShowerThoughts');
 
 hot_posts = subreddit.hot(limit=15)
-post_number = random.randint(0, 14)
 
-for i, submission in enumerate(hot_posts):
-   print(submission.title, i)
-#   if i == post_number:
-#      print(submission.url)
-
-#for submission in hot_python:
-#   if not submission.stickied:
-#      print(submission.title)
+for submission in hot_posts:
+   if not submission.stickied:
+      print(submission.title)
