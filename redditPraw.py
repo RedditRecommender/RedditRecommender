81212572b75fd09cd2a97449a6f08cf0aab4db86
The Asian Next Door <you@example.comls>
2017-11-21 18:55:20 -0600

    test

--- redditPraw.py
+++ redditPraw.py
@@ -6,17 +6,7 @@ subreddit = reddit.subreddit('ShowerThoughts');
 
 hot_posts = subreddit.hot(limit=15)
 
-for i, submission in enumerate(hot_posts):
-	#stuff
-   print(submission.title, i) #test
-#   if i == post_number:
-#      print(submission.url)
-
-#for submission in hot_python:
-#   if not submission.stickied:
-#      print(submission.title)
-
-
-#sast#sast
 for submission in hot_posts:
    if not submission.stickied:
+      print(submission.title)
+
