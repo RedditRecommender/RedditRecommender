97ea81c44be99186056bdef7cdba97b750e7b369
maliciousbanjo <joebill79@gmail.com>
2017-11-21 21:05:14 -0600

    Pulled random users from random subreddits

--- redditPraw.py
+++ redditPraw.py
@@ -1,20 +1,23 @@
-#!/usr/bin/python3
+#! /usr/local/bin/python3.5
 import praw
-from credentials import reddit
+import random
 
-'''
-subreddit = reddit.subreddit('ShowerThoughts');
+reddit = praw.Reddit(client_id = 'rPCwo0Eool5JHw', 
+                     client_secret = 'm8fD_XxXnUJYnyscLahHjGPMVPU',
+                     username = 'RNGesusBOT',
+                     password = 'phonebottlebattery',
+                     user_agent = 'RedditScraper')
+
+subreddit = reddit.subreddit('dankmemes');
 
 hot_posts = subreddit.hot(limit=15)
+post_number = random.randint(0, 14)
 
-for submission in hot_posts:
-   if not submission.stickied:
-      print(submission.title)
-'''
-# Waddup nerds here's how to grab some user info
-username = 'spez' # set whatever username you want here
+for i, submission in enumerate(hot_posts):
+   print(submission.title, i)
+#   if i == post_number:
+#      print(submission.url)
 
-myRedditor = reddit.redditor(username) # Create a 'Redditor' object from that username
-for comment in myRedditor.comments.new(limit=10): # Get that user's comments
-   print(comment.body.split('\n', 1)[0][:79]) # The comment
-   print('Subreddit: ', comment.subreddit.display_name) # The subreddit the comment is on
+#for submission in hot_python:
+#   if not submission.stickied:
+#      print(submission.title)
