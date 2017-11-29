b6c2dcf3b48355f310d18fcbeeaa0890a723f59a
The Asian Next Door <you@example.comls>
2017-11-28 21:17:59 -0600

    fixed subredditList Add

--- Version1.py
+++ Version1.py
@@ -2,52 +2,57 @@
 import praw
 from credentials import reddit
 
+#ID for users and subreddits to be used in Map dicitonaries
+userIDCounter = 0
+subredditIDCounter = 0
+
 #dictionary for map of Users to ID
 userMap = {}
 
 #dictionary for map of subreddit to ID
 subredditMap = {}
 
-#ID for users and subreddits to be used in Map dicitonaries
-userNumber = 0
-subNumber = 0
+#dictionary for users and subreddits
+userSubsDict = {}
 
 #list of users
 userList = set()
 
-#dictionary for users and subreddits
-userSubsDict = {}
-
-#finds the redditors subreddits through their comments
-subList = set()
+#list of subreddits that have commented on
+subredditListMaster = set()
 
 #finds random subredits then gathers users in said subreddit
-for subreddit in range(1):
+for subreddit in range(2):
     sub = (reddit.subreddit('random'))
-    for comments in sub.comments(limit = 10):
+    for comments in sub.comments(limit = 5):
         userList.add(comments.author)
 
 #finds subreddits the users have commented on
 for user in userList:
     redditor = str(user)
+    #list of subreddits specific to user
+    subredditRedditorList = set()
     #number of comments for specific user
     numberOfComments = 0
     #finds the subreddit each comment resides in
-    for comment in reddit.redditor(redditor).comments.new(limit=3):
-        subRedditName = str(comment.subreddit.display_name)
-        #checks if subreddit is in list or not
-        if subRedditName not in subList:
+    for comment in reddit.redditor(redditor).comments.new(limit=4):
+        subredditName = str(comment.subreddit.display_name)
+        #checks if subreddit is in Master list or not
+        if subredditName not in subredditListMaster:
             numberOfComments += 1
-            subList.add(subRedditName)
+            subredditListMaster.add(subredditName)
             #assigns subreddit to subredditMap dictionary with ID
-            subredditMap[subRedditName] = subNumber
-            subNumber += 1
+            subredditMap[subredditName] = subredditIDCounter
+            subredditIDCounter += 1
+        #checks if subreddit is in user specific list or not
+        if subredditName not in subredditRedditorList:
+            subredditRedditorList.add(subredditName)
     #adds redditor only if they have comments
     if numberOfComments > 0:
         #assigns user to userMap dictionary with ID
-        userMap[redditor] = userNumber
-        userSubsDict[redditor] = subList
-        userNumber += 1
+        userMap[redditor] = userIDCounter
+        userSubsDict[redditor] = subredditRedditorList
+        userIDCounter += 1
 
 #writes mapped values to file
 file = open("data.txt", "w")
@@ -55,8 +60,8 @@ for user,subredditList in userSubsDict.items():
     #loops through subredditList
     for item in subredditList:
         userID = userMap[user]
-        subRedditID = subredditMap[item]
-        line = "{} {}".format(userID, subRedditID)
+        subredditID = subredditMap[item]
+        line = "{} {}".format(userID, subredditID)
         file.write(line + '\n')
 file.close();
 
@@ -67,12 +72,10 @@ for user,ID in userMap.items():
 file.close()
 
 #writes subredditMap to file
-file = open("subRedditMap.txt", "w")
-for subReddit,ID in subredditMap.items():
-    file.write("{} {}\n".format(subReddit, ID))
+file = open("subredditMap.txt", "w")
+for subreddit,ID in subredditMap.items():
+    file.write("{} {}\n".format(subreddit, ID))
 file.close()
-
-
 
 print(userSubsDict)
 print(userMap)
